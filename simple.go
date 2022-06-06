3b838e679c532a6a8489eb7dcfb25087c0a5650c
elbek-ganiev <twobealfa@yandex.ru>
2022-06-06 15:45:25 +0300

    feature/Finished realization of simple Fibonacci

--- simple.go
+++ simple.go
@@ -1,1 +1,22 @@
-package fibonacciUsual
+package main
+
+import "fmt"
+
+func main() {
+	x1 := 0
+	x2 := 1
+	for x := 0; x < 10; x++ {
+		if x == 0 {
+			fib := 0
+			fmt.Println(fib)
+		} else if x == 1 {
+			fib := 1
+			fmt.Println(fib)
+		} else {
+			fib := x1 + x2
+			x1 = x2
+			x2 = fib
+			fmt.Println(fib)
+		}
+	}
+}
